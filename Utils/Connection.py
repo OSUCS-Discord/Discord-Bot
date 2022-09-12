674f7921634d3a4c2ab0f08b6d3af7d73b0a42da
Alex Henner <jahenner@gmail.com>
2022-09-12 12:56:47 -0400

    Connection is now good to go!

--- Utils/Connection.py
+++ Utils/Connection.py
@@ -4,17 +4,30 @@ import os
 import discord
 from dotenv import load_dotenv
 
-# load in the .env variables for use ASK ME FOR TOKEN
-load_dotenv()
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-
-# Setting intents
-intents = discord.Intents.default()
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord')
+class connection:
+    # I hate this, but the async function can't have self
+    global _client
+    _intents = discord.Intents.default()
+    _client = discord.Client(intents=_intents)
+        
+    def __init__(self) -> None:
+        self._TOKEN = self._get_token()
+        
+    def _get_token(self):
+        # load in the .env variables for use ASK ME FOR TOKEN
+        load_dotenv()
+        return os.getenv("DISCORD_BOT_TOKEN")
     
-client.run(TOKEN)
+    @_client.event
+    async def on_ready():
+        print(f'{_client.user} has connected to Discord')
+    
+    def run(self):
+        _client.run(self._TOKEN)
+        
+def main():
+    connect = connection()
+    connect.run()
+    
+if __name__ == "__main__":
+    main()
--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
 # Main bot code
+from Utils.Connection import connection
+
+def main():
+    bot = connection()
+    bot.run()
+    
+if __name__ == "__main__":
+    main()
